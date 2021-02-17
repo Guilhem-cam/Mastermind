32cdd2f538d05d10a76fdd116950f683659fca3b
Guilhem Cambon <gcambo01@pcalb-mm0000.univ-jfc.fr>
2021-02-17 12:01:03 +0100

    début

--- src/mastermind/Modèle.java
+++ src/mastermind/Modèle.java
@@ -1,5 +1,26 @@
 package mastermind;
 
-public class Modèle {
+import java.awt.Color;
+import java.util.ArrayList;
 
+public class Modèle {
+	Color[] COULEURS= new Color[7];
+	int DIFFICULTE;
+	enum Etat {ENCOURS,GAGNEE,PERDU};
+	
+	
+	public Modèle() {
+		this.COULEURS[0]=Color.yellow;
+		this.COULEURS[1]=Color.green;
+		this.COULEURS[2]=Color.blue;
+		this.COULEURS[3]=Color.red;
+		this.COULEURS[4]=Color.magenta;
+		this.COULEURS[5]=Color.orange;
+		this.COULEURS[6]=Color.white;
+		this.COULEURS[7]=Color.black;
+		
+		this.DIFFICULTE=0;
+		
+	}
+	
 }
